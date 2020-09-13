45a5e093aff4a7ab9d5a60798339ae8c238320e1
Vlgdev <vig-dev@mail.ru>
2020-09-13 14:03:02 +0300

    Logout message

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -51,6 +51,7 @@
 
 <script>
 import { email, required, minLength } from "vuelidate/lib/validators";
+import messages from '@/utils/messages'
 
 export default {
   name: "Login",
@@ -66,6 +67,11 @@ export default {
     password: {
       required,
       minLength: minLength(6)
+    }
+  },
+  mounted() {
+    if (this.$route.query.message) {
+      this.$message(messages[this.$route.query.message])
     }
   },
   methods: {
